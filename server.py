db1ba69db3c453a6eabe6d7bb84d0bdd8f76fa20
KeeKelly <keekelly@outlook.com>
2018-01-20 19:51:24 -0400

    Added index.html

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print(request.get_json())
+    return app.send_static_file("index.html")
 
 if __name__ == '__main__':
     app.run()

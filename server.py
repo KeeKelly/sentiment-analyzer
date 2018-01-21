b24a32393fc191737d9c791b9066643aa131d49c
KeeKelly <keekelly@outlook.com>
2018-01-20 22:59:17 -0400

    Preparing for Heroku deployment

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = Flask(__name__, static_url_path='/static')
 
+
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
@@ -21,5 +22,6 @@ def get_sentiment():
     sentence = request.get_json()['input']
     return jsonify(sid.polarity_scores(sentence))
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
